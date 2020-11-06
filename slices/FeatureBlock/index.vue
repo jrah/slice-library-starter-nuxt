1e25f7bcbc70f1bf308ea4f2524b1b9718805c2b
James Home <james@jameshome.co.uk>
2020-11-06 01:07:38 +0000

    Add basic slice

--- slices/FeatureBlock/index.vue
+++ slices/FeatureBlock/index.vue
@@ -1,8 +1,24 @@
 <template>
-  <section class="section">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-  </section>
+  <div class="slice" :class="slice.slice_type">
+    <prismic-rich-text
+      :field="slice.primary.title"
+      class="title text-center text-4xl font-extrabold tracking-tight text-gray-800"
+    />
+    <prismic-rich-text
+      :field="slice.primary.subheading"
+      class="subheading mt-3 text-center max-w-4xl mx-auto"
+    />
+    <div class="mt-6 flex">
+      <div v-for="(item, i) in slice.items" :key="i" class="item">
+        <prismic-rich-text :field="item.subheading" class="subheading" />
+        <img :src="item.image.url" :alt="item.image.alt" />
+        <prismic-rich-text
+          :field="item.heading"
+          :class="`${slice.slice_type}` - `${item.heading[0].type}`"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -11,25 +27,9 @@ export default {
       type: Object,
       required: true,
       default() {
-        return {}
-      }
-    }
+        return {};
+      },
+    },
   },
-}
+};
 </script>
-<style scoped>
-.section {
-  position: relative;
-  background: #F7F7F7;
-  color: #111;
-  padding: 4em;
-  text-align: center;
-}
-a {
-  color: #111;
-}
-.title {
-  margin-bottom: 2em;
-}
-</style>
-
