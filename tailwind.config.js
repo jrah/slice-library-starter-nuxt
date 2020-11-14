d0ce3c08d7cb547ad52ecb1bc1a1f091da023375
James Home <james@jameshome.co.uk>
2020-11-14 00:58:05 +0000

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: { 
+        'primary': '#1F1620'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
