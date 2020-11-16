ec2d861f32189a1d32bc094fda8f589ac3ff3baa
James Home <james@jameshome.co.uk>
2020-11-16 18:36:21 +0000

    Add basic splash

--- slices/SplashDisplay/index.vue
+++ slices/SplashDisplay/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <section class="section">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-  </section>
+  <div :class="slice.slice_type">
+    <div class="lg:grid lg:grid-cols-2">
+      <prismic-rich-text :field="slice.primary.content" :class="`${slice.slice_type}-content`" class="self-center pb-12 lg:pb-0 lg:pr-12"/>
+      <prismic-image v-if="slice.primary.image" :field="slice.primary.image" />
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -11,25 +13,21 @@ export default {
       type: Object,
       required: true,
       default() {
-        return {}
-      }
-    }
+        return {};
+      },
+    },
   },
-}
+};
 </script>
-<style scoped>
-.section {
-  position: relative;
-  background: #F7F7F7;
-  color: #111;
-  padding: 4em;
-  text-align: center;
-}
-a {
-  color: #111;
-}
-.title {
-  margin-bottom: 2em;
+<style lang="scss" scoped>
+.splash_display {
+  &-content {
+    /deep/ h3 {
+      @apply text-4xl;
+      @apply font-extrabold;
+      @apply tracking-tight;
+      @apply text-gray-800;
+    }
+  }
 }
 </style>
-
