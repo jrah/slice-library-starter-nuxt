27a699cb9c31ce1e1dc959ce5aaf71caa659e63a
James Home <james@jameshome.co.uk>
2020-11-17 21:32:47 +0000

    Add container class to layout tmp

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,15 @@
     <Nuxt />
   </div>
 </template>
+
+<style lang="scss">
+@import "~/assets/scss/utils/mixins";
+
+.container {
+  @apply px-8;
+  @include mq(sm) {
+    @apply px-24;
+  }
+  @extend %mwc;
+}
+</style>
