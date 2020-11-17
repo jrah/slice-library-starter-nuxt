a99b6ae0d84cfe2337b595d21e337a6c6f6f2a35
James Home <james@jameshome.co.uk>
2020-11-17 21:46:15 +0000

    Add container

--- slices/SplashDisplay/index.vue
+++ slices/SplashDisplay/index.vue
@@ -1,13 +1,24 @@
 <template>
   <div :class="slice.slice_type">
-    <div class="sm:flex sm:items-center sm:justify-between" :class="childOrderReverse ? 'flex-row-reverse' : false">
-      <prismic-rich-text
-        :field="slice.primary.content"
-        :class="`${slice.slice_type}-content`"
-        class="sm:w-1/2 w-full"
-      />
-      <div :class="childOrderReverse ? 'sm:pr-4' : 'sm:pl-4'" class="sm:w-1/2 w-full">
-      <prismic-image v-if="slice.primary.image" :field="slice.primary.image" />
+    <div class="container">
+      <div
+        class="sm:flex sm:items-center sm:justify-between"
+        :class="childOrderReverse ? 'flex-row-reverse' : false"
+      >
+        <prismic-rich-text
+          :field="slice.primary.content"
+          :class="`${slice.slice_type}-content`"
+          class="sm:w-1/2 w-full"
+        />
+        <div
+          :class="childOrderReverse ? 'sm:pr-4' : 'sm:pl-4'"
+          class="sm:w-1/2 w-full"
+        >
+          <prismic-image
+            v-if="slice.primary.image"
+            :field="slice.primary.image"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -25,12 +36,12 @@ export default {
   },
   computed: {
     childOrderReverse() {
-      if(this.slice.primary.direction === 'default') {
-        return false
+      if (this.slice.primary.direction === "default") {
+        return false;
       }
-      return true
-    }
-  }
+      return true;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
