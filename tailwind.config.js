ba8fd264709c60c9eec3ac62bc674c486f263ddc
James Home <james@jameshome.co.uk>
2020-11-19 21:26:39 +0000

    Fix tailwind classes being purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    mode: 'layers',
+    layers: [''],
+
+  },
   theme: {
     extend: {
       colors: { 
