d8f25c0885a2bdbc9e2e6e45f50678c1123f528d
James Home <james@jameshome.co.uk>
2020-11-21 02:10:06 +0000

    Setup basic carousel

--- slices/CarouselDisplay/index.vue
+++ slices/CarouselDisplay/index.vue
@@ -1,8 +1,57 @@
 <template>
-  <section class="section">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-  </section>
+  <div class="slice" :class="slice.slice_type">
+    <div class="container">
+      <div class="flex flex-col-reverse sm:flex-row relative px-6">
+        <button
+          :disabled="this.maxPrevElementArray"
+          class="cursor-pointer w-6 h-6 absolute inset-y-1/2 left-0 bg-secondary"
+          @click="showPrevElement()"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <div class="w-full sm:w-1/2 flex">
+          <prismic-rich-text
+            :class="`${slice.slice_type}-content`"
+            class="self-center p-4"
+            :field="currentElement.content"
+          ></prismic-rich-text>
+        </div>
+        <div class="w-full sm:w-1/2">
+          <prismic-image
+            class="object-fit rounded-lg"
+            :field="currentElement.image"
+          ></prismic-image>
+        </div>
+        <button
+          :disabled="this.maxNextElementArray"
+          class="cursor-pointer w-6 h-6 absolute inset-y-1/2 right-0 bg-secondary rounded-md"
+          @click="showNextElement()"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -11,25 +60,41 @@ export default {
       type: Object,
       required: true,
       default() {
-        return {}
-      }
-    }
+        return {};
+      },
+    },
   },
-}
+  data() {
+    return {
+      currentElementIndex: 0,
+    };
+  },
+  computed: {
+    currentElement() {
+      return this.slice.items[this.currentElementIndex];
+    },
+    maxPrevElementArray() {
+      return this.currentElementIndex === 0;
+    },
+    maxNextElementArray() {
+      return this.currentElementIndex === this.slice.items.length - 1;
+    },
+  },
+  methods: {
+    showNextElement() {
+      this.currentElementIndex++;
+    },
+    showPrevElement() {
+      this.currentElementIndex--;
+    },
+  },
+};
 </script>
-<style scoped>
-.section {
-  position: relative;
-  background: #F7F7F7;
-  color: #111;
-  padding: 4em;
-  text-align: center;
-}
-a {
-  color: #111;
-}
-.title {
-  margin-bottom: 2em;
+
+<style lang="scss">
+.carousel_display {
+  :disabled {
+    @apply opacity-25;
+  }
 }
 </style>
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   theme: {
     extend: {
+      inset: {
+        '1/2': '50%'
+      },
       colors: { 
         // http://colormind.io/#:~:text=If%20no%20colors%20are%20locked%20Colormind%20will%20generate,them%20at%20the%20opposite%20ends%20of%20the%20palette.
         'primary': '#1F1620',
