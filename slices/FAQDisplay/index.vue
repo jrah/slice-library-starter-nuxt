0909d774b69da52935f4383d91c6bf36bbdeb2f2
James Home <james@jameshome.co.uk>
2020-11-23 02:53:53 +0000

    Add FAQ section

--- slices/FAQDisplay/index.vue
+++ slices/FAQDisplay/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <section class="section">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-  </section>
+  <div class="container">
+    <prismic-rich-text
+      :field="slice.primary.title"
+        class="text-center text-4xl font-extrabold tracking-tight text-gray-800"
+    />
+    <div class=" container mt-12 flex flex-wrap">
+      <div v-for="(item, i) in slice.items" :key="i" class="w-1/3 p-4">
+        <prismic-rich-text
+          :field="item.heading"
+          class="font-sans text-lg leading-6 font-bold text-primary"
+        />
+        <prismic-rich-text
+          :field="item.paragraph"
+          class="mt-3 leading-6 text-gray-700"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -11,25 +25,10 @@ export default {
       type: Object,
       required: true,
       default() {
-        return {}
-      }
-    }
+        return {};
+      },
+    },
   },
-}
+};
 </script>
-<style scoped>
-.section {
-  position: relative;
-  background: #F7F7F7;
-  color: #111;
-  padding: 4em;
-  text-align: center;
-}
-a {
-  color: #111;
-}
-.title {
-  margin-bottom: 2em;
-}
-</style>
 
