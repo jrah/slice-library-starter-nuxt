bf782a229a08ea965746eb813685aecc8d08c467
James Home <james@jameshome.co.uk>
2021-01-17 19:44:45 +0000

    Use bg-primary

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -34,7 +34,7 @@
           <div class="inline-flex rounded-md shadow">
             <nuxt-link
               to="/"
-              class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded md text-indigo-600 bg-white hover:bg-gray-50"
+              class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded md text-white bg-primary hover:bg-gray-50"
             >
               Create account
             </nuxt-link>
